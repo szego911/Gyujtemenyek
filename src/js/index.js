7a7490419233c9fe159bdac73f8ea4ddcd11cda5
Szegedi Bence <benceszegedi43@gmail.com>
2025-03-01 18:35:05 +0100

    -rename collection function, dialog endpoints -rename item function, dialog endpoints -localstorage for data -deafault image use -cleaning up the code

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,7 @@
-let collections = [];
-let items = [];
-
 const url = "http://localhost:8080";
+
 var selectedCollectionId = -1;
+
 const cardsContainer = document.querySelector(".cards-container");
 const heroContainer = document.querySelector(".hero");
 const collectionHero = document.querySelector(".collectionHero");
@@ -10,18 +9,23 @@ const selectedCollectionElement = document.querySelector(
   ".selected-collection"
 );
 const loadingSpinner = document.getElementById("loading");
-const collectionWindow = document.getElementById("newCollectionWindow");
 const itemWindow = document.getElementById("newItemWindow");
 const successCollectionAlert = document.getElementById(
   "success-collection-alert"
 );
 const successItemAlert = document.getElementById("success-item-alert");
+const successDeleteItemAlert = document.getElementById(
+  "success-delete-item-alert"
+);
+const renameCollAlert = document.getElementById("success-renameColl-alert");
+const renameItemAlert = document.getElementById("success-renameItem-alert");
+const moveItemAlert = document.getElementById("success-move-item-alert");
 
 async function fetchCollections() {
   try {
     let response = await fetch(url + "/collections");
     let data = await response.json();
-    collections = data;
+    localStorage.setItem("collections", JSON.stringify(data));
   } catch (error) {
     console.error("Hiba történt az adatok lekérésekor:", error);
   }
@@ -31,27 +35,40 @@ async function fetchItems() {
   try {
     let response = await fetch(url + "/items");
     let data = await response.json();
-    items = data;
+    localStorage.setItem("items", JSON.stringify(data));
   } catch (error) {
     console.error("Hiba történt az adatok lekérésekor:", error);
   }
 }
 
 export const renderCard = () => {
+  const collections = JSON.parse(localStorage.getItem("collections"));
   collections.map((collection) => {
     const cardElement = document.createElement("div");
+    const renameId = "rename" + collection.name;
     cardElement.innerHTML = `
     <div class="card p-1 card-width">
-        <img src="./src/assets/images/Diplomatico.png" class="card-img-top" alt="..."/>
+        <img src="./src/assets/images/Default.png" class="card-img-top" alt="..."/>
         <div class="card-body">
             <h5 class="card-title">${collection.name}</h5>
             <p class="card-text">${collection.theme}</p>
             <button id="${collection.name}" class="btn bg-mozaik text-white">Megnyitás</button>
+            <button id="${renameId}" class="btn bg-secondary position-absolute end-0 me-4 text-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear-fill" viewBox="0 0 16 16">
+                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+              </svg>
+            </button>
         </div>
     </div>
     `;
 
     cardsContainer.appendChild(cardElement);
+
+    const renameBtn = document.getElementById(renameId);
+    renameBtn?.addEventListener("click", () => {
+      renameCollDialog(collection.name);
+    });
+
     const button = document.getElementById(collection.name);
     button?.addEventListener("click", () => {
       openCollection(collection.name);
@@ -59,16 +76,95 @@ export const renderCard = () => {
   });
 };
 
-function addOrUpdateURLParam(key, value) {
-  const searchParams = new URLSearchParams(window.location.search);
-  searchParams.set(key, value);
-  const newRelativePathQuery =
-    window.location.pathname + "?" + searchParams.toString();
-  history.pushState(null, "", newRelativePathQuery);
+function renameCollDialog(collectionName) {
+  $("#reNameModalLabel").html(collectionName + " átnevezése");
+  $("#newCollName").val(collectionName);
+  $("#renameCollDialog").modal("show");
+}
+
+function renameItemDialog(itemName) {
+  $("#reNameItemModalLabel").html(itemName + " átnevezése");
+  $("#newItemName").val(itemName);
+  $("#reNameItemDialog").modal("show");
+}
+
+async function renameCollection() {
+  const label = document.getElementById("reNameModalLabel").innerHTML;
+  const myArray = label.split(" ");
+  let collectionName = myArray[0];
+  let collectionId = "";
+  let collections = JSON.parse(localStorage.getItem("collections"));
+  collections.map((collection) => {
+    if (collection.name === collectionName) {
+      collectionId = collection._id;
+    }
+  });
+  const newName = $("#newCollName").val();
+
+  if (newName !== "" && typeof newName === "string") {
+    await fetch(url + "/update/collection", {
+      method: "PATCH",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: newName,
+        collectionId: collectionId,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          renderCard();
+          renameCollAlert.style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+}
+
+async function renameItem() {
+  const label = document.getElementById("reNameItemModalLabel").innerHTML;
+  const myArray = label.split(" ");
+  let itemName = myArray[0];
+  let itemId = "";
+  let items = JSON.parse(localStorage.getItem("items"));
+  items.map((item) => {
+    if (item.name === itemName) {
+      itemId = item._id;
+    }
+  });
+
+  const newName = $("#newItemName").val();
+
+  if (newName !== "" && typeof newName === "string") {
+    await fetch(url + "/update/item", {
+      method: "PATCH",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: newName,
+        itemId: itemId,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          renderCard();
+          renameItemAlert.style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
 
 function openCollection(collectionName) {
-  //addOrUpdateURLParam("collection", collectionName);
+  let collections = JSON.parse(localStorage.getItem("collections"));
   const selectedCollection = collections.find(
     (collection) => collection.name == collectionName
   );
@@ -76,7 +172,10 @@ function openCollection(collectionName) {
 
   cardsContainer.style.display = "none";
   heroContainer.style.display = "none";
-  //collectionWindow.style.display = "none";
+
+  document.getElementById("collectionNameH").innerHTML = collectionName;
+
+  let items = JSON.parse(localStorage.getItem("items"));
 
   items.map((item) => {
     if (item.collectionId == selectedCollectionId) {
@@ -86,7 +185,7 @@ function openCollection(collectionName) {
       const moveID = "moveBTN" + item.name;
       collectionItem.innerHTML = `
     <div class="card itemCard p-3">
-        <img src="./src/assets/images/Diplomatico.png" class="card-img-top" alt="..."/>
+        <img src="./src/assets/images/Default.png" class="card-img-top" alt="..."/>
         <div class="card-body">
             <h5 class="card-title">${item.name}</h5>
             <div class="btn-group">
@@ -121,11 +220,10 @@ function openCollection(collectionName) {
       const moveBtn = document.getElementById(moveID);
 
       deleteBtn.addEventListener("click", () => {
-        console.log("click");
         deleteItem(item.name);
       });
       updateBtn.addEventListener("click", () => {
-        // TODO: open a form for updating the item's name
+        renameItemDialog(item.name);
       });
       moveBtn.addEventListener("click", () => {
         // TODO: move
@@ -138,70 +236,79 @@ function openCollection(collectionName) {
 }
 
 export async function insertCollection() {
-  //TODO: validate data
   const name = document.getElementById("name").value;
   const theme = document.getElementById("theme").value;
   const date = document.getElementById("date").value;
 
-  let collectionId = 0;
-  collections.map((collection) => {
-    if (collection.collectionId > collectionId) {
-      collectionId = collection.collectionId;
-    }
-  });
-  collectionId += 1;
-
-  await fetch(url + "/insertcollection", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      theme: theme,
-      date: date,
-      collectionId: collectionId,
-    }),
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        successCollectionAlert.style.display = "block";
-        renderCard();
+  if (
+    (name !== "" || theme !== "" || date !== "") &&
+    typeof name !== "string" &&
+    typeof theme !== "string"
+  ) {
+    let collectionId = 0;
+    let collections = JSON.parse(localStorage.getItem("collections"));
+    collections.map((collection) => {
+      if (collection.collectionId > collectionId) {
+        collectionId = collection.collectionId;
       }
-    })
-    .catch((error) => {
-      console.error(error);
     });
+    collectionId += 1;
+
+    await fetch(url + "/insertcollection", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        theme: theme,
+        date: date,
+        collectionId: collectionId,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          renderCard();
+          successCollectionAlert.style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    alert("Nem jol megadott paraméterek");
+  }
 }
 export function insertItem() {
-  //TODO: validate data
   const name = document.getElementById("itemName").value;
-  const collectionId = selectedCollectionId;
-  fetch(url + "/insertitem", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      name: name,
-      collectionId: collectionId,
-    }),
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        successItemAlert.style.display = "block";
-        renderCard();
-      }
+  if (name !== "" && typeof name === "string") {
+    const collectionId = selectedCollectionId;
+    fetch(url + "/insertitem", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        collectionId: collectionId,
+      }),
     })
-    .catch((error) => {
-      console.error(error);
-      // Handle any errors here
-    });
+      .then((response) => {
+        if (response.status === 200) {
+          renderCard();
+          successItemAlert.style.display = "block";
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } else {
+    alert("Nem jól megadott paraméterek");
+  }
 }
 
-export function deleteCollection(collectionName) {
-  console.log("lefut");
-  fetch(url + "/deletecollection", {
+/*export function deleteCollection(collectionName) {
+  fetch(url + "/delete/collection", {
     method: "DELETE",
     headers: {
       "Content-type": "application/json",
@@ -214,10 +321,10 @@ export function deleteCollection(collectionName) {
       console.log("Sikeresen törölve!");
     }
   });
-}
+}*/
+
 export function deleteItem(itemName) {
-  console.log("lefut");
-  fetch(url + "/deleteitem", {
+  fetch(url + "/delete/item", {
     method: "DELETE",
     headers: {
       "Content-type": "application/json",
@@ -227,7 +334,8 @@ export function deleteItem(itemName) {
     }),
   }).then((response) => {
     if (response.status == 200) {
-      console.log("Sikeresen törölve!");
+      renderCard();
+      successDeleteItemAlert.style.display = "block";
     }
   });
 }
@@ -240,9 +348,15 @@ document
   ?.addEventListener("click", insertCollection);
 
 document.getElementById("newItemButton")?.addEventListener("click", insertItem);
-document.body.addEventListener("load", renderCard);
+document
+  .getElementById("renameCollButton")
+  ?.addEventListener("click", renameCollection);
+
+document
+  .getElementById("renameItemButton")
+  ?.addEventListener("click", renameItem);
 
 setTimeout(() => {
   loadingSpinner.style.display = "none";
   renderCard();
-}, 1000);
+}, 600);
--- src/js/server.js
+++ src/js/server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const { connectToDb, getDb } = require("./db");
 const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 let db;
+const url = "http://localhost:8080";
 
 connectToDb((err) => {
   if (!err) {
-    app.listen(8080, () => {
+    app.listen(8080, "0.0.0.0", () => {
       console.log("Connected to the database");
-      //index.renderCard();
     });
     db = getDb();
   }
@@ -30,7 +31,7 @@ app.get("/collections", (req, res) => {
       res.status(200).json(collections);
     })
     .catch((err) => {
-      //res.status(500).json(err);
+      res.status(500).json(err);
     });
 });
 
@@ -46,11 +47,9 @@ app.get("/items", (req, res) => {
       res.status(200).json(items);
     })
     .catch((err) => {
-      //res.status(500).json(err);
+      res.status(500).json(err);
     });
 });
-
-//TODO: create UD endpoints
 
 app.post("/insertcollection", async (req, res) => {
   let collection = await db.collection("collections");
@@ -76,19 +75,28 @@ app.post("/insertitem", async (req, res) => {
   }
 });
 
-// Update the post with a new comment
-app.patch("/updatecollection/:name", async (req, res) => {
-  const query = { _id: ObjectId(req.params.name) };
+app.patch("/update/collection", async (req, res) => {
+  const query = { _id: new ObjectId(req.body.collectionId.toString()) };
   const updates = {
-    $push: { comments: req.body },
+    $set: { name: req.body.name },
   };
   let collection = await db.collection("collections");
   let result = await collection.updateOne(query, updates);
-  res.send(result).status(200);
+  res.status(200);
 });
 
-// Delete an entry
-app.delete("/deleteitem", async (req, res) => {
+app.patch("/update/item", async (req, res) => {
+  const query = { _id: new ObjectId(req.body.itemId) };
+  const updates = {
+    $set: { name: req.body.name },
+  };
+
+  let collection = await db.collection("items");
+  let result = await collection.updateOne(query, updates);
+  res.status(200);
+});
+
+app.delete("/delete/item", async (req, res) => {
   const name = req.body.name;
   const collection = db.collection("items");
   let result = await collection.deleteOne({ name: name });
